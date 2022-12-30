ab34d8f5cbe2334bc4d096e01c9c2190073f579b
Tanaka, Teppei <s2110100@jaist.ac.jp>
2022-12-30 20:40:36 +0900

    backend POST, DELETE, PUT メソッドを追加

--- api/src/Todo.hs
+++ api/src/Todo.hs
@@ -13,6 +13,7 @@ import GHC.Generics(Generic)
 import Network.Wai
 import Network.Wai.Logger
 import Network.Wai.Handler.Warp
+import Network.Wai.Middleware.Cors
 import Database.PostgreSQL.Simple
 import Database.PostgreSQL.Simple.FromRow
 import Control.Monad.IO.Class
@@ -27,10 +28,10 @@ connectInfo = ConnectInfo
     }
 
 initDB :: Connection -> IO Int64
-initDB conn = execute_ conn "CREATE TABLE IF NOT EXISTS todos (id integer, comment varchar(100))"
+initDB conn = execute_ conn "CREATE TABLE IF NOT EXISTS todos (id Int, comment varchar(100))"
 
 data Todo = Todo
-  { id :: Integer
+  { id :: Int
   , comment :: String
   } deriving (Show, Generic)
 
@@ -42,33 +43,47 @@ instance FromRow Todo where
 
 type API = "todo" :> ReqBody '[JSON] Todo :> Post '[JSON] NoContent
   :<|> "todo" :> Get '[JSON] [Todo]
-  :<|> "todo" :> Capture "id" Integer :> Delete '[JSON] NoContent
+  :<|> "todo" :> Capture "id" Int :> Delete '[JSON] NoContent
+  :<|> "todo" :> ReqBody '[JSON] Todo :> Put '[JSON] NoContent
 
 api :: Proxy API
 api = Proxy
 
 server :: ConnectInfo -> Server API
-server ci = postTodo :<|> getTodo :<|> deleteTodo
+server ci = postTodo :<|> getTodo :<|> deleteTodo :<|> putTodo
   where
     postTodo :: Todo -> Handler NoContent
-    postTodo (Todo num com) = do
+    postTodo (Todo n txt) = do
       liftIO . withConnect ci $ \conn ->
-        execute conn "INSERT INTO todos VALUES (?,?)" (num,com)
+        execute conn "INSERT INTO todos VALUES (?,?)" (n,txt)
       return NoContent
 
     getTodo :: Handler [Todo]
-    getTodo = fmap (map (\(num,com) -> Todo num com)) . liftIO $
+    getTodo = fmap (map (\(n,txt) -> Todo n txt)) . liftIO $
      withConnect ci $ \conn ->
         query_ conn "SELECT id,comment FROM todos"
 
-    deleteTodo :: Integer -> Handler NoContent
+    deleteTodo :: Int -> Handler NoContent
     deleteTodo n = do
       liftIO . withConnect ci $ \conn ->
         execute conn "DELETE FROM todos WHERE id = ?" (Only n)
       return NoContent
 
+    putTodo :: Todo -> Handler NoContent
+    putTodo (Todo n txt) = do
+      liftIO . withConnect ci $ \conn ->
+        execute conn "UPDATE todos SET comment = ? WHERE id = ?" (txt,n)
+      return NoContent
+
 withConnect :: ConnectInfo -> (Connection -> IO c) -> IO c
 withConnect ci = bracket (connect ci) close
+
+todoCors :: Middleware
+todoCors = cors (const $ Just policy)
+  where
+    policy = simpleCorsResourcePolicy
+      { corsRequestHeaders = ["Content-Type"]
+      , corsMethods = ["GET", "POST", "DELETE", "PUT"] }
 
 runServant :: IO ()
 runServant = do
@@ -76,4 +91,4 @@ runServant = do
   initDB conn
   withStdoutLogger $ \aplogger -> do
     let settings = setPort 3100 (setLogger aplogger defaultSettings)
-    runSettings settings (serve api (server connectInfo))
+    runSettings settings (todoCors (serve api (server connectInfo)))
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import React, { useState } from 'react'
 
 export async function getServerSideProps() {
   const res = await fetch(`http://127.0.0.1:3100/todo`, {method: 'GET'})
@@ -9,11 +9,76 @@ export async function getServerSideProps() {
   return { props: { data } }
 }
 
+export function postAPI(props, text) {
+  const res = fetch(`http://127.0.0.1:3100/todo`, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({id: props.data.length + 1, comment: text})
+  });
+
+  return res;
+}
+
+export function deleteAPI(n) {
+  const res = fetch(`http://127.0.0.1:3100/todo/${n}`, {
+    method: 'DELETE',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+
+  return res;
+}
+
+export function putAPI(n,text) {
+  const res = fetch(`http://127.0.0.1:3100/todo/`, {
+    method: 'PUT',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({id: n, comment: text})
+  })
+
+  return res;
+}
+
 export default function Home(props) {
-  const table_define = [
+  const tableDefine = [
     {label: 'id', key: 'id'},
     {label: 'comment', key: 'comment'}
   ];
+
+  const [todo, setTodo] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const postResponse = await postAPI(props, todo);
+    console.log(postResponse);
+    window.location.reload();
+  }
+
+  function handleChangeSubmit(e) {
+    setTodo(e.target.value)
+  }
+
+  async function handleDelete(e,n) {
+    e.preventDefault();
+    const deleteResponse = await deleteAPI(n);
+    console.log(deleteResponse);
+    window.location.reload();
+  }
+
+  async function handlePut(e,n,text) {
+    e.preventDefault();
+    const putResponse = await putAPI(n,text);
+    console.log(putResponse);
+    window.location.reload();
+  }
 
   return (
     <>
@@ -28,9 +93,10 @@ export default function Home(props) {
           <table>
             <thead>
               <tr>
-                {table_define.map((def) => (
+                {tableDefine.map((def) => (
                   <th>{def.label}</th>
                 ))}
+                <th>-</th>
                 <th>-</th>
               </tr>
             </thead>
@@ -39,7 +105,8 @@ export default function Home(props) {
                   <tr>
                     <th key={row.id}>{row.id}</th>
                     <td key={row.comment}>{row.comment}</td>
-                    <td> <button>削除</button></td>
+                    <td> <button onClick={(e) => handleDelete(e,row.id)}>削除</button></td>
+                    <td> <button onClick={(e) => handlePut(e,row.id, '編集テスト')}>編集</button> </td>
                   </tr>
                 ))}
             </tbody>
@@ -48,9 +115,9 @@ export default function Home(props) {
 
         <div className={styles.center}>
         <h2>新しいTodoの追加</h2>
-          <form>
-            内容 <input id="new_todo" type="text"/>
-            <button>追加</button>
+          <form onSubmit={handleSubmit}>
+            内容 <input id="newTodo" name="newTodo" value={todo} onChange={handleChangeSubmit} type="text"/>
+            <button type="submit">追加</button>
           </form>
         </div>
       </main>
