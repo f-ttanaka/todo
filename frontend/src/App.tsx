8a97a37a029c662bb0f048603019d2a4666535c0
f-ttanaka <ttanaka394@gmail.com>
2025-03-29 15:15:31 +0900

    decide to use vite for frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,55 +1,18 @@
-import { useEffect, useState } from 'react'
-import { useTodos } from './hooks/api'
-import type { Todo } from './hooks/api'
-import FilterButton from './components/FilterButton'
-import Form from './components/Form'
-import TodoTemplate from './components/Todo'
+import { routeTree } from './routeTree.gen';
+import { RouterProvider, createRouter } from '@tanstack/react-router';
 
-type Filter = 'All' | 'Active' | 'Completed'
+const router = createRouter({ routeTree: routeTree });
 
-const App = () => {
-  const [filter, setFilter] = useState<Filter>('All')
-  const [todos, setTodos] = useState<Todo[] | null>(null)
-  console.log(todos)
-  const data = useTodos()
-  useEffect(() => {
-    data.then((ts) => setTodos(ts))
-  }, [])
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
 
-  console.log(data);
-
-  const filteredTodos = todos?.filter((todo) => {
-    switch (filter) {
-      case 'All':
-        return true
-      case 'Active':
-        return !todo.completed
-      case 'Completed':
-        return todo.completed
-    }
-  })
-
+function App() {
   return (
-    <div className='todoapp stack-large'>
-      <h1>TodoMatic</h1>
-      <Form />
-      <div className='filters btn-group stack-exception'>
-        <FilterButton label='All' onClick={() => setFilter('All')} />
-        <FilterButton label='Active' onClick={() => setFilter('Active')} />
-        <FilterButton label='Completed' onClick={() => setFilter('Completed')} />
-      </div>
-      <h2 id='list-heading'>{filteredTodos?.length ?? 0} tasks remaining</h2>
-      <ul
-        role='list'
-        className='todo-list stack-large stack-exception'
-        aria-labelledby='list-heading'
-      >
-        {filteredTodos?.map(({ title, uuid, completed }) => (
-          <TodoTemplate key={uuid} title={title} uuid={uuid} completed={completed} />
-        ))}
-      </ul>
-    </div>
-  )
+      <RouterProvider router={router} />
+  );
 }
 
 export default App;
